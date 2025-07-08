78f9d6fc5fd18a41b1b0ab444d4213757af029eb
Rensvdk <rensvdk@gmail.com>
2025-07-08 21:06:13 +0200

    Added recipes to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
 	<NuxtLayout>
-		<div>Home</div>
+		<div class="max-w-[1200px] mx-auto px-4 py-8">
+			<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+				<Recipe />
+			</div>
+		</div>
 	</NuxtLayout>
 </template>
