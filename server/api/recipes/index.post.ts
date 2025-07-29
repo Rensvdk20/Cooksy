997d19ed1e77055be0f66c832dfdd0a4107fcb3d
Rensvdk <rensvdk@gmail.com>
2025-07-29 13:55:36 +0200

    Added create recipe

---
+++ server/api/recipes/index.post.ts
@@ -0,0 +1,45 @@
+import prisma from "~/lib/prisma";
+
+import { EditRecipe, editRecipeSchema } from "~/schemas/recipe/editRecipeSchema";
+
+export default defineEventHandler(async (event) => {
+	await auth(event);
+
+	const body = await readBody(event);
+	const parsedRecipe = editRecipeSchema.safeParse(body);
+
+	if (!parsedRecipe.success) {
+		console.error(parsedRecipe.error.issues);
+
+		throw createError({
+			statusCode: 400,
+			statusMessage: parsedRecipe.error.issues.map(issue => issue.message).join(', ')
+		});
+	}
+
+	const recipe: EditRecipe = parsedRecipe.data;
+
+	await prisma.recipe.create({
+		data: {
+			name: recipe.name,
+			servings: recipe.servings,
+			main_img: recipe.main_img,
+			steps: {
+				create: recipe.steps.map((step) => ({
+					name: step.name,
+					instructions: step.instructions,
+				})),
+			},
+			ingredientBlocks: {
+				create: recipe.ingredientBlocks.map((block) => ({
+					name: block.name,
+					ingredients: {
+						create: block.ingredients.map((ingredient) => ({
+							name: ingredient.name,
+						})),
+					},
+				})),
+			},
+		},
+	});
+});
