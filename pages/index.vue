89fb2482ca53f004dd7fabab23576cebe29a3bce
Rensvdk <rensvdk@gmail.com>
2025-07-30 22:06:45 +0200

    Fixed layout rendering twice

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,9 @@ const recipes = await $fetch<Recipe[]>('/api/recipes');
 </script>
 
 <template>
-	<NuxtLayout>
-		<div class="max-w-[1200px] mx-auto px-4 py-8">
-			<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
-				<RecipeItem v-for="recipe in recipes" :recipe :key="recipe.id" />
-			</div>
+	<div class="max-w-[1200px] mx-auto px-4 py-8">
+		<div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+			<RecipeItem v-for="recipe in recipes" :recipe :key="recipe.id" />
 		</div>
-	</NuxtLayout>
+	</div>
 </template>
