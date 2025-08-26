9aa41cc58fc0d76041e47d89123735aa890e0cdd
Rensvdk <rensvdk@gmail.com>
2025-08-26 14:33:05 +0200

    Changed save/edit buttons location on mobile

--- pages/recipes/create.vue
+++ pages/recipes/create.vue
@@ -150,6 +150,14 @@ function addStep() {
 						/>
 					</div>
 				</div>
+				<div class="lg:hidden mb-2">
+					<button @click="createRecipe" class="flex w-full justify-center gap-1 border border-neutral-700 p-2 rounded-3xl cursor-pointer hover:bg-neutral-800 transition-all">
+						<svg class="w-5" viewBox="0 0 640 640" fill="white">
+							<path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 237.3C544 220.3 537.3 204 525.3 192L448 114.7C436 102.7 419.7 96 402.7 96L160 96zM192 192C192 174.3 206.3 160 224 160L384 160C401.7 160 416 174.3 416 192L416 256C416 273.7 401.7 288 384 288L224 288C206.3 288 192 273.7 192 256L192 192zM320 352C355.3 352 384 380.7 384 416C384 451.3 355.3 480 320 480C284.7 480 256 451.3 256 416C256 380.7 284.7 352 320 352z"/>
+						</svg>
+						Save
+					</button>
+				</div>
 				<div class="mb-2">
 					<h5 class="text-lg mb-1">Recipe name</h5>
 					<input v-model="recipe.name" type="text" class="w-full rounded-sm p-1 px-2 border border-neutral-700" placeholder="Name" />
@@ -209,7 +217,7 @@ function addStep() {
 			<div class="col-span-2 p-4 bg-zinc-800 rounded-2xl">
 				<div class="flex items-center justify-between mb-4">
 					<h2 class="text-2xl mb-2">Steps</h2>
-					<button @click="createRecipe" class="flex gap-1 border border-neutral-700 p-2 rounded-3xl cursor-pointer hover:bg-neutral-800 transition-all">
+					<button @click="createRecipe" class="lg:flex hidden gap-1 border border-neutral-700 p-2 rounded-3xl cursor-pointer hover:bg-neutral-800 transition-all">
 						<svg class="w-5" viewBox="0 0 640 640" fill="white">
 							<path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 237.3C544 220.3 537.3 204 525.3 192L448 114.7C436 102.7 419.7 96 402.7 96L160 96zM192 192C192 174.3 206.3 160 224 160L384 160C401.7 160 416 174.3 416 192L416 256C416 273.7 401.7 288 384 288L224 288C206.3 288 192 273.7 192 256L192 192zM320 352C355.3 352 384 380.7 384 416C384 451.3 355.3 480 320 480C284.7 480 256 451.3 256 416C256 380.7 284.7 352 320 352z"/>
 						</svg>
